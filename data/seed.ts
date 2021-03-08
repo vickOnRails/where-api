e2d0253694fc5b096623b3413ccd86477d8ed954
vickonrails <victorofoegbu0009@gmail.com>
2021-03-09 00:20:06 +0100

    refactor: database seed script

--- data/seed.ts
+++ data/seed.ts
@@ -2,25 +2,54 @@ import { PrismaClient } from "@prisma/client";
 import states from "./states.json";
 import lgas from "./lgas.json";
 
-const pool = new PrismaClient();
+const prisma = new PrismaClient();
+
+const ALL = `--all`;
+const STATES = `--states`;
+const LGAS = `--lgas`;
 
 const main = async () => {
+  const { argv } = process;
+
+  if (argv[2] === ALL) {
+    await seedNGStates();
+    await seedNGLGAs();
+  }
+  if (argv[2] === STATES) {
+    await seedNGStates();
+  }
+  if (argv[2] === LGAS) {
+    await seedNGLGAs();
+  }
+};
+
+const seedNGLGAs = async () => {
+  console.log(`Seeding Nigerian Local Govts`);
   try {
-    // await pool.state.deleteMany();
-    await pool.lGA.deleteMany();
-
-    // const newStates = await pool.state.createMany({
-    //   data: states,
-    // });
-
-    const newLGAs = await pool.lGA.createMany({
+    await prisma.lGA.deleteMany();
+    const newLGAs = await prisma.lGA.createMany({
       data: lgas,
     });
 
-    // console.log(newStates);
-    // console.log(newLGAs);
+    console.log(`Successfully seeded Nigerian LGAs`);
+    console.table(newLGAs);
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
+  }
+};
+
+const seedNGStates = async () => {
+  console.log(`Seeding Nigerian States`);
+  try {
+    await prisma.state.deleteMany();
+    const newStates = await prisma.state.createMany({
+      data: states,
+    });
+
+    console.log(`Successfully seeded Nigerian states`);
+    console.table(newStates);
+  } catch (err) {
+    console.log(err);
   }
 };
 
@@ -32,6 +61,6 @@ main()
     throw err;
   })
   .finally(async () => {
-    await pool?.$disconnect();
+    await prisma?.$disconnect();
     process.exit(0);
   });
